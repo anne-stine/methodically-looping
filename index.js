dc5bd8d069f2dd88d4d644997a4b9f9acef69e69
Anne-Stine Gjervoldstad <annestine.kodehode@gmail.com>
2022-02-23 19:48:07 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+//#region --- Methodically Looping
+
+// 1. Find a piece of text and check if it is above a specified wordcount (signs).
+let lorem = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa officia tenetur ullam, eligendi sunt unde in amet! Numquam fugit aliquid dolorem necessitatibus, exercitationem odit pariatur dolores, voluptate minima qui quas. Qui iusto harum in dolorem velit itaque blanditiis voluptas nostrum, laborum exercitationem ipsum ratione fugit amet sequi. Optio error soluta dicta vitae nesciunt, quae explicabo reiciendis doloribus voluptas rerum, quisquam facilis odio accusamus sequi, quidem amet cumque animi quos illum distinctio numquam iusto nobis perferendis. Quidem autem, numquam cumque tempore non necessitatibus, ducimus iusto quia sequi ut facilis, ipsum iste? Facere doloremque soluta dolor quibusdam eligendi deleniti placeat nulla a."
+
+let wordCount = lorem.length(50)
+console.log(wordCount)
+
+// 2. Make a loop that counts to 10.
+
+
+// 3. Find the words "if". "as" and "and" in your text and remove these words, plus 5 signs on each side of the words.
+
+
+// 4. Merge your new strings.
+
+
+// 5. Replace those words with Norwegian words.
+
+
+// 6. Create a little game where the player has 3 lives. Hind: while loops.
+
+
+// 7. Make an array with 10 words and one with 10 numbers.
+
+
+// 8. Make all the numbers the same, except the first and last numbers, one number at a time.
+
+
+//#endregion
