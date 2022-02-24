5e0dcdaaa84640089a80cb2540cf2478ab7b773e
Anne-Stine Gjervoldstad <annestine.kodehode@gmail.com>
2022-02-24 14:07:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 //#region --- Methodically Looping
 
-// 1. Find a piece of text and check if it is above a specified wordcount (signs).
-let lorem = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa officia tenetur ullam, eligendi sunt unde in amet! Numquam fugit aliquid dolorem necessitatibus, exercitationem odit pariatur dolores, voluptate minima qui quas. Qui iusto harum in dolorem velit itaque blanditiis voluptas nostrum, laborum exercitationem ipsum ratione fugit amet sequi. Optio error soluta dicta vitae nesciunt, quae explicabo reiciendis doloribus voluptas rerum, quisquam facilis odio accusamus sequi, quidem amet cumque animi quos illum distinctio numquam iusto nobis perferendis. Quidem autem, numquam cumque tempore non necessitatibus, ducimus iusto quia sequi ut facilis, ipsum iste? Facere doloremque soluta dolor quibusdam eligendi deleniti placeat nulla a."
 
-let wordCount = lorem.length(50)
-console.log(wordCount)
+// 1. Find a piece of text and check if it is above a specified wordcount (signs).
+let text = "Lorem ipsum, dolor if amet consectetur adipisicing and. Voluptates molestias as animi nulla architecto voluptatibus reprehenderit and tempore dolores quidem."
+
+// console.log(text.length > 500)
+
 
 // 2. Make a loop that counts to 10.
+// for (let i = 1; i < 11; i++) {
+//     console.log(i)
+// }
 
 
 // 3. Find the words "if". "as" and "and" in your text and remove these words, plus 5 signs on each side of the words.
+// do while (text.includes("if")) {
+//     text.slice(-5, 10)
+//     console.log(text)
+// }
+
+let indexOfAnd = text.indexOf("and")
+let newText = " "
+
+for (let i = indexOfAnd - 5; i < indexOfAnd + 6; i++) {
+    newText += text.substring(i)
+    console.log(newText)
+}
+
+console.log(newText)
 
 
 // 4. Merge your new strings.
 
 
 // 5. Replace those words with Norwegian words.
-
+// console.log(text.replace("old word", "new word"))
 
 // 6. Create a little game where the player has 3 lives. Hind: while loops.
 
